35eba5dfecb0ba29ab5c9dcce00539ccbd02df62
xWisecrack <64139907+xWisecrack@users.noreply.github.com>
2022-07-19 16:39:39 +0600

    Delete Lesson 1 directory

--- Lesson 1/task_3.py
+++
@@ -1,9 +0,0 @@
-# 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3.
-# Считаем 3 + 33 + 333 = 369.
-
-user_n = input('Введите число ')
-n = int(user_n)
-nn = int(user_n * 2)
-nnn = int(user_n * 3)
-result = n + nn + nnn
-print(f'Вы ввели {user_n}. {n} + {nn} + {nnn} = {result}')
